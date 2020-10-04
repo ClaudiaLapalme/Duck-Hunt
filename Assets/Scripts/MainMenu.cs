a8fbc1a0e6a4c78d2f9669eed60a067a8a65a7e9
Claudia <claudia.lapalme@gmail.com>
2020-10-04 18:05:05 -0400

    Made functional main menu + main menu music

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private AudioSource source;
+    [SerializeField] private AudioClip clickSound;
+    
+    public void PlayGame()
+    {
+        StartCoroutine(DelayedPlayGame());
+    }
+
+    private IEnumerator DelayedPlayGame()
+    {
+        yield return new WaitForSeconds(clickSound.length + 0.25f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        StartCoroutine(DelayedQuitGame());
+    }
+
+    private IEnumerator DelayedQuitGame()
+    {
+        yield return new WaitForSeconds(clickSound.length);
+        Debug.Log("Game has been quit");
+        Application.Quit();
+    }
+    
+    public void OnClick()
+    {
+        source.PlayOneShot(clickSound);
+    }
+}
