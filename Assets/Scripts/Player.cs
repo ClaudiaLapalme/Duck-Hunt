052d58d60d2c140f2b002ce9c5aacf21a4603a72
Claudia <claudia.lapalme@gmail.com>
2020-10-05 11:06:12 -0400

    Added the player and mouse controlled movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        if (cam == null)
+        {
+            cam = Camera.main;
+        }
+
+        if (cam is null) return;
+        
+        var mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        var targetPos = new Vector3(mousePos.x, mousePos.y, 0);
+
+        targetPos = ClampedPlayerPosition(targetPos);
+        
+        GetComponent<Rigidbody2D>().MovePosition(targetPos);
+    }
+
+    Vector3 ClampedPlayerPosition(Vector3 targetPos)
+    {
+        var screenLimits = new Vector3(Screen.width, Screen.height, 0);
+        var outerLimit = cam.ScreenToWorldPoint(screenLimits);
+        var maxHeight = outerLimit.y - GetComponentInChildren<Renderer>().bounds.extents.y;
+        var maxWidth = outerLimit.x - GetComponentInChildren<Renderer>().bounds.extents.x;
+
+        var lockedPosY = Mathf.Clamp(targetPos.y, -maxHeight, maxHeight);
+        var lockedPosX = Mathf.Clamp(targetPos.x, -maxWidth, maxWidth);
+        
+        return new Vector3(lockedPosX, lockedPosY, 0);
+    }
+}
